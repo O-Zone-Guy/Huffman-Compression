9a55aee761e02da7b4455b8659763060f49bf91f
omar <oalkersh@ymail.com>
2018-11-22 21:08:30 -0500

    Added frequency func, returns the frequency of elements in a list

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,20 @@
 module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Numeric
+import Data.Char
+
+data Comp a = Leaf a
+            | Branch a (Comp a)
+
+test :: IO ()
+test = putStrLn "Hello World"
+
+-- returns a list of tuples containing the frequency of an element and the element
+frequency :: Eq a => [a] -> [(Int, a)]
+frequency xs = let
+ unique xs [] = xs
+ unique xs (y:ys) | y `elem` xs = unique xs ys
+                  | otherwise   = unique (y:xs) ys
+ count xs [] = []
+ count xs (u:us) = (length $ filter (==u) xs,u):count xs us
+ in count xs (unique [] xs)
