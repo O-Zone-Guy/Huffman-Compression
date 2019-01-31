b5521d86e8da22249976a5690321a508fd937f8e
omar <oalkersh@ymail.com>
2019-01-31 12:17:52 -0500

    added compress list func that takes a list and compresses it

--- src/Lib.hs
+++ src/Lib.hs
@@ -36,3 +36,14 @@ sortedToHuff :: [(Int, a)] -> Comp a
 sortedToHuff [] = error "Empty list"
 sortedToHuff [x] = Leaf $ snd x
 sortedToHuff (x:xs) = Branch (snd x) (sortedToHuff xs)
+
+compressList :: Eq a => [a] -> [Int]
+compressList xs = let
+ -- comp :: Eq a => [a] -> Int -> Comp a -> [Int]
+ comp [] _ _ = []
+ comp (x:xs) n (Leaf a)     | a == x    = n : comp xs 0 h
+                            | otherwise = error "Wrong Comp data"
+ comp (x:xs) n (Branch a c) | x == a    = n: comp xs 0 h
+                            | otherwise = comp (x:xs) (n+1) h
+ h = sortedToHuff $ order $ frequency xs
+ in comp xs 0 h
